e4f07c98390bb34147e622d95a1eb4fb8ba76d1b
Hariyanti16 <hariyanti.0645@students.amikom.ac.id>
2021-01-18 01:55:22 +0700

    Hariyanti 18.12.0645 UAS

---
+++ app/src/main/java/com/github_0645/api/data/model/User.kt
@@ -0,0 +1,6 @@
+package com.github_0645.api.data.model
+
+data class User(
+    val login: String,
+    val avatar_url: String
+)
---
+++ app/src/main/java/com/github_0645/api/data/model/UserResponse.kt
@@ -0,0 +1,5 @@
+package com.github_0645.api.data.model
+
+data class UserResponse(
+    val items : ArrayList<User>
+)
